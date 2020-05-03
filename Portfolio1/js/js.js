176748d70c3bca8cc1ddabe72845905a0f88449f
toysheeyeyraku <kirpich1337228@gmail.com>
2020-05-03 12:45:18 +0300

    2

---
+++ Portfolio1/js/js.js
@@ -0,0 +1,74 @@
+﻿$(function () {
+    $('#homeCarousel').carousel({
+        interval: 5000,
+        pause: "false"
+    });
+    $('#slowButton').click(function () {
+        c = $('#homeCarousel')
+        opt = c.data()['bs.carousel'].options
+        opt.interval = 10000;
+        c.data({ options: opt })
+        
+    });
+    $('#fastButton').click(function () {
+        c = $('#homeCarousel')
+        opt = c.data()['bs.carousel'].options
+        opt.interval = 1000;
+        c.data({ options: opt })
+    });
+   
+    
+});
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 60.8) {
+            $("#trub").removeClass("D");
+        } else {
+            $("#trub").addClass("D");
+        }
+    });
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer'
+    });
+
+    // layout Masonry after each image loads
+   
+    $(".sld").click(function () {
+
+        var c = parseInt( $(this).attr("data-slide-to"));
+        $('#homeCarousel').carousel(c);
+    });
+   
+    $("#firstc").addClass("active");
+    $(".collapseManager").click(function () {
+      
+        var cc = $(this).attr("data-target");
+        if ($(this).attr("aria-expanded") == "true") {
+            return;
+        }
+      
+        var elements = document.getElementsByClassName("collapseManager");
+        elements.forEach(element => {
+            if (element.getAttribute("data-target") != cc) {
+               
+                var c = element.getAttribute("data-target");
+                $(c + "z").removeClass("arrw2");
+               
+                $(c).collapse("hide");
+                element.setAttribute("aria-expanded", "false");
+               
+            }
+           
+
+        }
+        );
+        
+        $(cc).collapse("show");
+        $(cc).attr("aria-expanded", "true");
+        var e = cc + "z";
+        $(e).addClass("arrw2");
+    });
+    
+});
---
+++ Portfolio2/js/main.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function () { 
+   
+    $('.sl').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false
+    });
+    $('.sli').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        customPaging: function (slick, index) {
+            
+            return '<button class="tz" type="button" role="tab" id="slick-slide-control10" aria-controls="slick-slide10" aria-label="1 of 2" tabindex="0" aria-selected="true">1</button>';
+        }
+    });
+     $('.grid').masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer'
+    });
+    
+    $(window).resize(function () {
+
+        $('.sl').slick('slickSetOption', 'slidesToShow', 1);
+        if ($(window).width() >= 512) {
+            $('.sl').slick('slickSetOption', 'slidesToShow', 2);
+        }
+        if ($(window).width()>=768) {
+            $('.sl').slick('slickSetOption', 'slidesToShow', 3);
+        }
+        if ($(window).width() <= 992) {
+            $('.grid').masonry('destroy');
+            $('.grid').masonry({
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                columnWidth: '.grid-sizer'
+            });
+        }
+        if ($(window).width() <= 512) {
+            $('.grid').masonry('destroy');
+            $('.grid').masonry({
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                columnWidth: '.grid-sizer'
+            });
+        }
+    });
+    
+    $('.sl').slick('slickSetOption', 'slidesToShow', 1);
+    if ($(window).width() >= 512) {
+        $('.sl').slick('slickSetOption', 'slidesToShow', 2);
+    }
+    if ($(window).width() >= 768) {
+        $('.sl').slick('slickSetOption', 'slidesToShow', 3);
+    }
+    
+});
